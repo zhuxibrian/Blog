60428e5e16cb87623ec2c8e95578da3a30336d51
zhuxibrian <zhuxibrian@icloud.com>
2017-03-13 12:42:21 +0800

    自动化部署3

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
-git pull origin master
+#!/bin/bash
+ 
+WEB_PATH='/usr/hexo'
+ 
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
 echo "Finished."
