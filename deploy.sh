b57ee840afd3121d98fcd19491a742459ab16123
zhuxibrian <zhuxibrian@icloud.com>
2017-03-14 11:26:08 +0800

    修改自动化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,9 @@
- #!/bin/bash
-
-WEB_PATH='/usr/hexo/'$1
-WEB_USER='root'
-WEB_USERGROUP='root'
-
+#!/bin/bash
+ 
+WEB_PATH='/usr/hexo'
+ 
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
-git pull
-git checkout master
-echo "changing permissions..."
-chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+git pull origin master
 echo "Finished."
